42e27af6d7dbf39ce8ab685c6eeccfb42f9ba4de
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-06-01 22:20:28 -0400

    working homepage

--- frontend/yodlr/src/components/Home.js
+++ frontend/yodlr/src/components/Home.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Card, CardBody, CardTitle } from 'reactstrap';
+import React, { useContext } from 'react';
+import { Card, CardBody, CardTitle, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import UserContext from '../context/UserContext';
+import '../css/Home.css';
 
 const Home = () => {
+	const { users } = useContext(UserContext);
 	return (
 		<section className="col-md-8">
 			<Card>
-				<CardBody>
+				<CardBody className="text-center">
 					<CardTitle>
 						<b>Yodlr Design Challenge</b>
 					</CardTitle>
+					<span>A simple home for members like you.</span>
 				</CardBody>
 			</Card>
 		</section>
